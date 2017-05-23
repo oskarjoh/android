81c5224c9061c21827ae643a14a2b0fe015cf260
oskar <oskar@mangosia.com>
2017-05-23 06:52:06 +0200

    lagt til help som skriver lit om help

--- welcom.py
+++ welcom.py
@@ -3,7 +3,18 @@ import time
 
 print ("welcom")
 time.sleep(2)
-inputkey = input()
-while ( inputkey =! q)
-  print ("help = help quit = q")
-  if (inputkey == help)
+
+print ("")
+print ("")
+
+inputkey = ""
+while ( inputkey != "q"):
+  print ("help = h quit = q")
+  inputkey = input()
+  if (inputkey == "h"):
+    print ("")
+    print ("help")
+    print ("")
+    print ("første ting")
+    print ("")
+time.sleep(1)
