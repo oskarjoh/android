63a9e44f5d5d8c167fea25feca0315bcaa8d1102
oskarjoh <oskar@mangosia.com>
2017-05-23 05:56:01 +0200

    Update and rename k.py to welcom.py
    lagt til en while som skal søre en start serm

--- k.py
+++
@@ -1,6 +0,0 @@
-import random
-import time
-
-print ("welcom")
-time.sleep(2)
-if inputkey =! q
---
+++ welcom.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+print ("welcom")
+time.sleep(2)
+inputkey = input()
+while ( inputkey =! q)
+  print ("help = help quit = q")
+  if (inputkey == help)
